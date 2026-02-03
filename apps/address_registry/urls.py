dd55e4b5410e9575de234ba1a2a5bd955a84e439
Martynas Raila <martynas.raila@cornercasetech.com>
2026-02-03 13:22:17 +0200

    Add RC test endpoint with example files

--- apps/address_registry/urls.py
+++ apps/address_registry/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
-from apps.address_registry.views.rc_broker_views import get_data
+from apps.address_registry.views.rc_broker_views import get_data, rc_testing_view
 from apps.address_registry.views.sodra_views import skola_sodrai_view
 from apps.address_registry.views.views import (
     ContinentCountrySettlementViewSet,
@@ -50,6 +50,7 @@ urlpatterns = [
         ),
     ),
     re_path(r"^rc/get-data/", get_data),
+    re_path(r"^rc/testing/", rc_testing_view),
     re_path(r"^sodra/skola-sodrai/", skola_sodrai_view),
     path("<str:app_label>/<str:model_name>/generate/", GenerateTestData.as_view()),
     path("", include(router.urls)),
